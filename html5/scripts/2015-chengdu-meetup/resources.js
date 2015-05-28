3f579495659e6bacd389273ca93da78f542773ed
Hector Zhao <bzhao@mozilla.com>
2015-05-28 16:57:01 +0800

    wip

---
+++ html5/scripts/2015-chengdu-meetup/resources.js
@@ -0,0 +1,26 @@
+var audioFragments = [
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_audioinit.mp4",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_audio1.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_audio2.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_audio3.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_audio4.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_audio5.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_audio6.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_audio7.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_audio8.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_audio9.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_audio10.m4s",
+];
+
+var videoFragments = [
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_videoinit.mp4",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_video1.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_video2.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_video3.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_video4.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_video5.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_video6.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_video7.m4s",
+  "videos/2015-chengdu-meetup/mse-eme/sintel_trailer-720p-frag-cenc_video8.m4s",
+];
+
